d9e153c391f63c52c34a237f2ce2c25527e66f26
anfeliz79 <argenis1989@gmail.com>
2025-09-05 15:21:04 -0400

    Añadir soporte Dotenv y archivo de entorno

--- public/setup_admin.php
+++ public/setup_admin.php
@@ -1,6 +1,11 @@
 <?php
 require __DIR__ . "/../vendor/autoload.php";
 
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->safeLoad();
+
 use App\Config\Database;
 
 $db = new Database();
